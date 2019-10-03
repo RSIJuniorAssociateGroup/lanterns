0d0f134cf343e0cb413b1037e89d2bf097f3b7e9
Brandon Huffman <bt_huffman@msn.com>
2019-10-03 17:03:31 -0600

    Removed some comments

--- LanternsApp/LanternsApp/Models/Classes/LakeTile.cs
+++ LanternsApp/LanternsApp/Models/Classes/LakeTile.cs
@@ -7,8 +7,41 @@ namespace LanternsApp
 {
     public class LakeTile
     {
+        public static int TotalTilesCreated { get; set; } = 0;
 
+        public int TileId { get; set; }
 
+        //List of the 4 colors assigned to the tile
+        List<string> colorList = new List<string>();
+
+        //Constructor for creating a tile
+        public LakeTile(string colorZero, string colorOne, string colorTwo, string colorThree)
+        {
+            if (!CanCreateTile())
+            {
+                throw new Exception("You cannot create any more tiles, all 36 possible tiles have already been created.");
+            }
+            
+            TotalTilesCreated++;
+            TileId = TotalTilesCreated;
+            
+            colorList.Add(colorZero);
+            colorList.Add(colorOne);
+            colorList.Add(colorTwo);
+            colorList.Add(colorThree);
+        }
+
+        //Insuring can legally create tile
+        private bool CanCreateTile()
+        {
+            if (TotalTilesCreated <= 36)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
     }
 }
 
