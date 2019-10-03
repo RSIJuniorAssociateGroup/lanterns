2c51ea360f5a1c18635d6b5c05ba52e90a08b755
matthew.morgan <matthew.morgan@ruralsourcing.com>
2019-10-03 09:49:44 -0600

    Added lantern enum and added the colors for each lantern type.

---
+++ LanternsApp/LanternsApp/Lantern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanternsApp
+{
+    public enum Lantern
+    {
+        RED,
+        BLUE,
+        PURPLE,
+        GREEN,
+        WHITE,
+        BLACK,
+        YELLOW,
+    }
+}
