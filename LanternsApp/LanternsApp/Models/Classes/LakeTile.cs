85a51dafc923cd983304a953684499e5c960c286
Brandon Huffman <bt_huffman@msn.com>
2019-10-03 16:55:29 -0600

    Removed the console based unit test and minor adjustment to the .gitignore and an exception error

--- LanternsApp/LanternsApp/Models/Classes/LakeTile.cs
+++ LanternsApp/LanternsApp/Models/Classes/LakeTile.cs
@@ -7,41 +7,7 @@ namespace LanternsApp
 {
     public class LakeTile
     {
-        //Count lake tiles
-        public static int TotalTilesCreated { get; set; } = 0;
 
-        //Tile Id
-        public int TileId { get; set; }
-
-        List<string> colorList = new List<string>();
-
-        public LakeTile(string colorZero, string colorOne, string colorTwo, string colorThree)
-        {
-            if (!CanCreateTile())
-            {
-                throw new Exception("You cannot create any more tiles");
-            }
-            
-            TotalTilesCreated++;
-            TileId = TotalTilesCreated;
-            
-            colorList.Add(colorZero);
-            colorList.Add(colorOne);
-            colorList.Add(colorTwo);
-            colorList.Add(colorThree);
-            Console.WriteLine(colorList);
-        }
-
-        private bool CanCreateTile()
-        {
-            if (TotalTilesCreated <= 36)
-            {
-                return true;
-            } else
-            {
-                return false;
-            }
-        }
 
     }
 }
--- LanternsApp/LanternsApp/Program.cs
+++ LanternsApp/LanternsApp/Program.cs
@@ -13,14 +13,53 @@ namespace LanternsApp
 {
     public class Program
     {
+
+        //Count lake tiles
+        public static int TotalTilesCreated { get; set; } = 0;
+
+        //Tile Id
+        public int TileId { get; set; }
+
+        //List<string> colorList = new List<string>();
+
+        public void LakeTile(string colorZero, string colorOne, string colorTwo, string colorThree)
+        {
+            if (!CanCreateTile())
+            {
+                throw new Exception("You cannot create any more tiles");
+            }
+
+            TotalTilesCreated++;
+            TileId = TotalTilesCreated;
+
+
+            var colorList = new List<string>();
+            colorList.Add(colorZero);
+            colorList.Add(colorOne);
+            colorList.Add(colorTwo);
+            colorList.Add(colorThree);
+            Console.WriteLine(colorList);
+        }
+
+        private bool CanCreateTile()
+        {
+            if (TotalTilesCreated <= 36)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         public static void Main(string[] args)
         {
             //CreateWebHostBuilder(args).Build().Run();
             Console.WriteLine("Here is the start");
             Console.ReadLine();
             LakeTile tile00 = new LakeTile("red", "blue", "green", "yellow");
-            Console.WriteLine(tile00);
             JsonUtility.Dump(tile00);
+            Console.WriteLine();
             Console.ReadLine();
         }
 
