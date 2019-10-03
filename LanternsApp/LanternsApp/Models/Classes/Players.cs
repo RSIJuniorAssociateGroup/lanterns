fe56940607f893bb58dc7b2caa8c1b8ab46ea87b
Brandon Huffman <bt_huffman@msn.com>
2019-10-03 17:24:18 -0600

    Added the required properties for the Player. To remain within scope of this ticket I kept it as simple as possile. Considerable functionality is to be covered in future^Cickets.

---
+++ LanternsApp/LanternsApp/Models/Classes/Players.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanternsApp.Models.Classes
+{
+    public class Player
+    {
+        public int PlayerId { get; set; } = 0;
+
+        public string PlayerName { get; set; }
+
+        //Takes in the LakeTile.TileId int
+        List<int> lakeTileHand = new List<int>();
+
+
+    }
+}
