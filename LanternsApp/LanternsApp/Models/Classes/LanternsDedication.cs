7692ac0c2acd6fa3a59639f72f6021229ec605cc
matthew.morgan <matthew.morgan@ruralsourcing.com>
2019-10-07 14:51:19 -0600

    Made Lanterns dedication non-static and moved list creation logic outside of constructor. Added tests for LanternsDedication.

--- LanternsApp/LanternsApp/Models/Classes/LanternsDedication.cs
+++ LanternsApp/LanternsApp/Models/Classes/LanternsDedication.cs
@@ -5,31 +5,43 @@ using System.Threading.Tasks;
 
 namespace LanternsApp.Models.Classes
 {
-    public static class LanternsDedication
+    public class LanternsDedication
     {
-        public static List<int> OneOfEach { get; private set; }
-        public static List<int> ThreePair { get; private set; }
-        public static List<int> FourOfAKind { get; private set; }
+        public List<int> OneOfEach;
+        public List<int> ThreePair;
+        public List<int> FourOfAKind;
 
-        public static void SetNumberOfTokensByPlayerCount(int numberOfPlayers)
+        public LanternsDedication()
+        {
+            OneOfEach = new List<int>();
+            ThreePair = new List<int>();
+            FourOfAKind = new List<int>();
+
+        }
+
+        public void SetListsByNumberOfPlayers(int numberOfPlayers)
         {
             if (numberOfPlayers == 4)
             {
-                OneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 9, 10 };
-                ThreePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 8, 9 };
-                FourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 7, 8 };
+                OneOfEach.AddRange(new List<int>{ 5, 6, 7, 7, 8, 8, 9, 9, 10 });
+                ThreePair.AddRange(new List<int> { 5, 5, 6, 6, 7, 7, 8, 8, 9 });
+                FourOfAKind.AddRange(new List<int> { 4, 5, 5, 5, 6, 6, 7, 7, 8 });
             }
             else if (numberOfPlayers == 3)
             {
-                OneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 10 };
-                ThreePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 9 };
-                FourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 8 };
+                OneOfEach.AddRange(new List<int> { 5, 6, 7, 7, 8, 8, 9, 10 });
+                ThreePair.AddRange(new List<int> { 5, 5, 6, 6, 7, 7, 8, 9 });
+                FourOfAKind.AddRange(new List<int> { 4, 5, 5, 5, 6, 6, 7, 8 });
             }
             else if (numberOfPlayers == 2)
             {
-                OneOfEach = new List<int> { 5, 6, 7, 8, 9, 10 };
-                ThreePair = new List<int> { 5, 5, 6, 7, 8, 9 };
-                FourOfAKind = new List<int> { 4, 5, 5, 6, 7, 8 };
+                OneOfEach.AddRange(new List<int> { 5, 6, 7, 8, 9, 10 });
+                ThreePair.AddRange(new List<int> { 5, 5, 6, 7, 8, 9 });
+                FourOfAKind.AddRange(new List<int> { 4, 5, 5, 6, 7, 8 });
+            }
+            else
+            {
+                throw new Exception("Invalid player number.");
             }
         }
     }
---
+++ LanternsApp/LanternsAppTest/Tests/LanternsDedicationTest.cs
@@ -0,0 +1,31 @@
+﻿using LanternsApp.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LanternsAppTest.Tests
+{
+    public class LanternsDedicationTest
+    {
+        private readonly LanternsDedication dedication = new LanternsDedication();
+        
+        [Theory]
+        [InlineData(2, 6)]
+        [InlineData(3, 8)]
+        [InlineData(4, 9)]
+        public void TestOneOfEachLength(int numberOfPlayers, int expectedListSize)
+        {
+            dedication.SetListsByNumberOfPlayers(numberOfPlayers);
+            Assert.Equal(expectedListSize, dedication.FourOfAKind.Count);
+            Assert.Equal(expectedListSize, dedication.ThreePair.Count);
+            Assert.Equal(expectedListSize, dedication.OneOfEach.Count);
+        }
+
+        [Fact]
+        public void TestInvalidNumberOfPLayers()
+        {
+            Assert.Throws<Exception>(() => dedication.SetListsByNumberOfPlayers(0));
+        }
+    }
+}
