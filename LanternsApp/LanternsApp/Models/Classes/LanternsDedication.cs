11b6e0d715e55efe650bd3a2e2d05d2c433a7415
matthew.morgan <matthew.morgan@ruralsourcing.com>
2019-10-07 11:29:36 -0600

    Added base class for dedication tokens and instatiated the lists of points based on amount of players.

---
+++ LanternsApp/LanternsApp/Models/Classes/LanternsDedication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanternsApp.Models.Classes
+{
+    public class LanternsDedication
+    {
+        List<int> oneOfEach;
+        List<int> threePair;
+        List<int> fourOfAKind;
+
+        public LanternsDedication(int numberOfPlayers = 4)
+        {
+            if (numberOfPlayers == 4)
+            {
+                oneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 9, 10 };
+                threePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 8, 9 };
+                fourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 7, 8 };
+            } else if (numberOfPlayers == 3)
+            {
+                oneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 10 };
+                threePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 9 };
+                fourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 8 };
+            } else if (numberOfPlayers == 2)
+            {
+                oneOfEach = new List<int> { 5, 6, 7, 8, 9, 10 };
+                threePair = new List<int> { 5, 5, 6, 7, 8, 9 };
+                fourOfAKind = new List<int> { 4, 5, 5, 6, 7, 8 };
+            } else
+            {
+                throw new Exception("Improper amount of players");
+            }
+        }
+    }
+}
