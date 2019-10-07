27b2ed49d2c378e226ed38f439efbbc8a4a7af73
matthew.morgan <matthew.morgan@ruralsourcing.com>
2019-10-07 11:42:58 -0600

    Refactored class to be static with a static set method for the token point lists.

--- LanternsApp/LanternsApp/Models/Classes/LanternsDedication.cs
+++ LanternsApp/LanternsApp/Models/Classes/LanternsDedication.cs
@@ -5,32 +5,31 @@ using System.Threading.Tasks;
 
 namespace LanternsApp.Models.Classes
 {
-    public class LanternsDedication
+    public static class LanternsDedication
     {
-        List<int> oneOfEach;
-        List<int> threePair;
-        List<int> fourOfAKind;
+        public static List<int> OneOfEach { get; private set; }
+        public static List<int> ThreePair { get; private set; }
+        public static List<int> FourOfAKind { get; private set; }
 
-        public LanternsDedication(int numberOfPlayers = 4)
+        public static void SetNumberOfTokensByPlayerCount(int numberOfPlayers)
         {
             if (numberOfPlayers == 4)
             {
-                oneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 9, 10 };
-                threePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 8, 9 };
-                fourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 7, 8 };
-            } else if (numberOfPlayers == 3)
+                OneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 9, 10 };
+                ThreePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 8, 9 };
+                FourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 7, 8 };
+            }
+            else if (numberOfPlayers == 3)
             {
-                oneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 10 };
-                threePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 9 };
-                fourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 8 };
-            } else if (numberOfPlayers == 2)
+                OneOfEach = new List<int> { 5, 6, 7, 7, 8, 8, 9, 10 };
+                ThreePair = new List<int> { 5, 5, 6, 6, 7, 7, 8, 9 };
+                FourOfAKind = new List<int> { 4, 5, 5, 5, 6, 6, 7, 8 };
+            }
+            else if (numberOfPlayers == 2)
             {
-                oneOfEach = new List<int> { 5, 6, 7, 8, 9, 10 };
-                threePair = new List<int> { 5, 5, 6, 7, 8, 9 };
-                fourOfAKind = new List<int> { 4, 5, 5, 6, 7, 8 };
-            } else
-            {
-                throw new Exception("Improper amount of players");
+                OneOfEach = new List<int> { 5, 6, 7, 8, 9, 10 };
+                ThreePair = new List<int> { 5, 5, 6, 7, 8, 9 };
+                FourOfAKind = new List<int> { 4, 5, 5, 6, 7, 8 };
             }
         }
     }
